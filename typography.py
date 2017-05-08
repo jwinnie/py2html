888b1ad3e351948cf9a1d79b4f2f5243be34fd4f
J Winnie <git@stormdesign.us>
2017-05-07 18:46:40 -0700

    Create typography.py

---
+++ typography.py
@@ -0,0 +1,59 @@
+def title1(content):
+    
+    return "<h1 class='display-1'>{}</h1>".format(content)
+
+def title2(content):
+    
+    return "<h2 class='display-2'>{}</h2>".format(content)
+
+def title3(content):
+    
+    return "<h3 class='display-3'>{}</h3>".format(content)
+
+def title4(content):
+    
+    return "<h4 class='display-4'>{}</h4>".format(content)
+
+def title5(content):
+    
+    return "<h5 class='display-5'>{}</h5>".format(content)
+
+def title6(content):
+    
+    return "<h6 class='display-6'>{}</h6>".format(content)
+    
+def h1(content):
+    
+    return "<h1>{}</h1>".format(content)
+
+def h2(content):
+    
+    return "<h2>{}</h2>".format(content)
+
+def h3(content):
+    
+    return "<h3>{}</h3>".format(content)
+
+def h4(content):
+    
+    return "<h4>{}</h4>".format(content)
+
+def h5(content):
+    
+    return "<h5>{}</h5>".format(content)
+
+def h6(content):
+    
+    return "<h6>{}</h6>".format(content)
+    
+def important(content):
+    
+    return """<div class="lead">{}</div>""".format(content)
+    
+def small(content):
+
+    return "<small>{}</small>".format(content)
+    
+def p(content):
+
+    return "<p>{}</p>".format(content)
